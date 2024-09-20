78b5d0520c00bda884df722e59fe761b923b07d2
bouboucouscous <remi.coussypro@gmail.com>
2024-09-20 10:48:42 +0200

    Ajoutez des fichiers projet.

---
+++ tp 2/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace tp_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog openFolderDialog = new FolderBrowserDialog();
+   
+            if (openFolderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(openFolderDialog.SelectedPath);
+                textbox.Text = openFolderDialog.SelectedPath;
+                foreach (FileInfo file in directoryInfo.GetFiles())
+                {
+                    if (file.Extension == ".jpg")
+                    {
+                        listbox.Items.Add(file.FullName);
+                    }
+                }
+            }
+        }
+    }
+}
